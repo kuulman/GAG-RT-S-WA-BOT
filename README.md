d47699576db67f38b6c60c9156241ebaa0b66b04
kuulman <cocoiscoolman@gmail.com>
2025-08-10 02:29:46 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wa-stock-bot
 Whatsapp Bot for Grow a Garden Stock!
+
+Modules for this bot (not included in this file)
+- whatsapp-web.js
+- puppeteer-core
+- qrcode-terminal
+- dotenv (for token file)
+- express
+Full version see at package.json
+
+- Carz Ken -
--- index.js
+++ index.js
@@ -4,34 +4,77 @@ const qrcode = require('qrcode-terminal')
 const axios = require('axios')
 const { Client: discordClient, GatewayIntentBits } = require('discord.js')
 const { LocalAuth, Client } = require('whatsapp-web.js')
+const os = require('os');
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+// Check server usage
+// Ram Usage
+const UsedMemory = os.totalmem() - os.freemem(); // Get used memory 
+const UsedMemoryGB = (UsedMemory / (1024 * 1024 * 1024)).toFixed(2); // Convert to GB
+const memoryPercentage = ((UsedMemory / os.totalmem()) * 100).toFixed(2); // Calculate memory usage percentage
+// CPU Usage
+function getCPUUsageAsync() {
+    return new Promise(resolve => {
+        const startMeasure = process.cpuUsage();
+        const startTime = Date.now();
+
+        setTimeout(() => {
+            const endMeasure = process.cpuUsage(startMeasure);
+            const endTime = Date.now();
+            
+            const elapsedTime = (endTime - startTime) / 1000; // Convert to seconds
+            const totalCPUTime = (endMeasure.user + endMeasure.system) / 1000000; // Convert to seconds
+            
+            // Calculate percentage (single core)
+            let cpuPercentage = (totalCPUTime / elapsedTime) * 100;
+            
+            // Ensure the percentage doesn't exceed 100%
+            cpuPercentage = Math.min(cpuPercentage, 100);
+            
+            resolve(cpuPercentage.toFixed(2));
+        }, 1000);
+    });
+}
+
+
 // Whatsapp Setup
-const waClient = new Client({ //WA Client
+const waClient = new Client({  //WA Client
     authStrategy: new LocalAuth({clientId: 'GAG-STOCK'})
 })
 
-waClient.on('qr', (qr) => qrcode.generate(qr, { small: true }));
-waClient.on('authenticated', () => console.log('Client is authenticated'));
-waClient.on('ready', () => console.log('Client is ready!'));
+waClient.on('qr', (qr) => qrcode.generate(qr, { small: true })); // Generate QR Code
+waClient.on('authenticated', () => console.log('Client is authenticated')); // If QR Code scanned
+waClient.on('ready', () => console.log('Client is ready!')); // If this bot succesfully login to number
 waClient.on('message', async message =>{ // Function for checking messages
     const messageBody = message.body.trim().toLowerCase(); // Make it to lowercase
-    if (message.from == '120363402935518269@g.us') { // Check if the message is from a user
+    if (message.from == '120363419338640318@newsletter') { // Check if the message is from a selected group
      console.log(message.from + ' has sent you message: ', messageBody); // Write that message to console
     } 
-    if (message.body.toLowerCase() == "bot_authors") { // Bot authors
-        await message.reply(`*Credits of Info GAG's Bot* \n\nScripter: @kucingpintar \nMessage Builder: @owlhouse22 \n\n© Copyright Info GAG. All rights reserved. \nThis bot retrives data from vulcanvalues.com`) 
-        console.log("Berhasil mengirimkan jawaban untuk command bot_authors")
+    if (message.body.toLowerCase() == "bot_info") { // Bot authors commands
+        const thumbnail = MessageMedia.fromFilePath('./thumbnail.png')
+        await message.reply(thumbnail, { 
+            caption:'*Info GAG RT-S Bot Info*\n_Version 1.5 (Vulcano)_\n\n_Changes:_\n- New Command for Admin\n- Changes `bot_authors` to `bot_info` command and update the response\n- From now, bot will be send the rare stock to the Whatsapp Channels not to group again. Group will be shutdown after this bot updates.\n\nCreated by: _@kucingpintar & @owlhouse22_\n\n© Copyright Info GAG. All rights reserved. \nThis bot retrives data from vulcanvalues.com'
+        }) 
+        console.log("Berhasil mengirimkan jawaban untuk command bot_authors") // Credits Information
     }
-})
-waClient.initialize()
+    if (message.body.toLowerCase() == "server_usage") { // Server usage command
+        (async () => {
+            const cpuUsage = await getCPUUsageAsync(); // Call CPU Usage Function
+            const ramUsage = `*RAM Usage:* ${UsedMemoryGB} GB / ${memoryPercentage}%`; // Call RAM Usage Function
+            console.log(`📊 Server CPU and RAM Usage \n\n${ramUsage} (All memory usage in server. Include from OS) \n*CPU Usage:* ${cpuUsage}% (This bot CPU usage)`) // Server Usage Information
+            await message.reply(`📊 Server CPU and RAM Usage \n\n${ramUsage} (All memory usage in server. Include from OS) \n*CPU Usage:* ${cpuUsage}% (This bot CPU usage)`); // Send the server usage to WA Group
+        })()
+    }
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}\n Please wait. This gonna take less/more than 1 minute`));
+})
+waClient.initialize() // Execution code for WA Client
+
+app.listen(PORT, () => console.log(`Vulcano 1.5 || Server is running on port ${PORT} \nPlease wait. This gonna take less/more than 1 minute`)); // Sent this to console if succesfully connected to node.js
 
 // Discord Setup
-const dcClient = new discordClient({
+const dcClient = new discordClient({ // DC Client Config
     intents: [
      GatewayIntentBits.Guilds,
      GatewayIntentBits.GuildMessages,
@@ -39,17 +82,18 @@ const dcClient = new discordClient({
     ]
 })
 
-const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN; // Check .env or make new if you are clone this repo
 const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID_STOCKS;
 const WA_GROUP_ID = process.env.WA_GROUP_ID;
+const WA_CHANNELS_JID = process.env.WA_CHANNELS_JID;
 
 dcClient.on('ready', () => {
-    console.log(`Discord bot is ready`);
+    console.log(`Discord bot is ready`); // Write that to console if the DC Client is ready
 });
 
-dcClient.on('messageCreate', async (message) => {
+dcClient.on('messageCreate', async (message) => { // Message Event for the stock
     
-    const StockRole = [
+    const StockRole = [ // Role ID (Change it)
         '1401502243127164978',
         '1401502371804348556',
         '1401502463307022357',
@@ -70,7 +114,6 @@ dcClient.on('messageCreate', async (message) => {
 
     if (message.content) {
         contentTosend += `${message.content}\n`;
-        
     }
 
     message.embeds.forEach((embed, index) => {
@@ -80,7 +123,7 @@ dcClient.on('messageCreate', async (message) => {
              const now = new Date();
              const diff = targetTime - now;
 
-             if (diff <= 0) return 'just now'; // Sudah lewat
+             if (diff <= 0) return 'just now'; 
 
              const seconds = Math.floor(diff / 1000);
              const minutes = Math.floor(seconds / 60);
@@ -96,13 +139,13 @@ dcClient.on('messageCreate', async (message) => {
             if (!text) return '';
 
                 return text
-                // Hapus emoji Discord <:emoji:123456789> → ganti dengan "-"
+                // Delete Discord Emoji <:emoji:123456789> (to) → "-"
                 .replace(/<a?:\w+:\d+>/g, '-')
-                // Ubah Discord bold **SUNDRIED** jadi *SUNDRIED* (WA style)
+                // Change Discord Bold (example) **SUNDRIED** to WA Bold (example) *SUNDRIED*
                 .replace(/\*\*([^\*]+)\*\*/g, '*$1*')
                 .replace(/<t:(\d+):[Rr]>/g, (_, ts) => getRelativeTime(ts))
             };
-        function getCurrentTimestamp() { // Function for Timestamp
+        function getCurrentTimestamp() { // Function for Current Timestamp
             const now = new Date();
             const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
             const day = now.getDate();
@@ -117,7 +160,7 @@ dcClient.on('messageCreate', async (message) => {
         
         if (embed.fields?.length) {
          embed.fields.forEach(field => {
-            // Format setiap field dengan nama dan value
+            // Format for embed field (Name and value)
             contentTosend += `\n🔹 ${cleanText(field.name)}:\n${cleanText(field.value)}`;
         });
     }
@@ -125,17 +168,23 @@ dcClient.on('messageCreate', async (message) => {
     if (embed.description) {
       contentTosend += `${cleanText(embed.description)}`;
     }
-    contentTosend += `\n`; // Spasi antar embed
+    contentTosend += `\n`; // give space to every embed
     contentTosend += `\n> Timestamp: ${getCurrentTimestamp()}\n`;
     contentTosend += `© Copyright Info GAG \n`;
   });
 
-  const cleanedMessage = contentTosend.replace(/<@&\d+>/g, '').trim(); // Pembersih mentions
+  const cleanedMessage = contentTosend.replace(/<@&\d+>/g, '').trim(); // Clean the mentions
 
   // Kirim pesan ke WA
-  waClient.sendMessage(process.env.WA_GROUP_ID, cleanedMessage);
+  waClient.sendMessage(process.env.WA_CHANNELS_JID, cleanedMessage);
   console.log('*Berhasil Mengirim Pesan:*\n', cleanedMessage);
 });
 
 dcClient.login(DISCORD_TOKEN);
+
+// Additional Information
+// If you want change number or log out from whatsapp, you can delete the folder in `./wwebjs_auth` & `./wwebjs_cache` and run the bot again. 
+// It will ask you to scan the QR Code again.
+// If there is error `Cannot read properties of undefined (reading 'getChat')`, just restart the bot. 
+// Because this error is caused by the WA Client not ready yet when there is rare stock.
 
